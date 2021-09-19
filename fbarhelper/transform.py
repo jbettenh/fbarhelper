56c8afa73898dc07c43a83c1c341c0f314d9f693
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-09-19 22:11:57 +0200

    Added separator and selected columns

--- fbarhelper/transform.py
+++ fbarhelper/transform.py
@@ -3,9 +3,12 @@ import pandas
 
 
 def main():
-    df = pandas.read_csv('C:/code/python3/fbarhelper/tests/testdata/Transactions_999_123456789_20210622_092547.csv', delimiter=';', encoding='ISO-8859-1')
-    print(df)
-
+    df = pandas.read_csv('C:/code/python3/fbarhelper/tests/testdata/Transactions_1.csv',
+                         sep=';',
+                         encoding='ISO-8859-1',
+                         parse_dates=['Buchungstag']
+                         )
+    print(df[['Buchungstag', 'Verwendungszweck', 'Soll', 'Waehrung']])
 
 if __name__ == '__main__':
     main()
