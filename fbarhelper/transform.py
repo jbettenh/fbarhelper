80257cd16163e8ce0e50a1a5ce963d244d171466
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-09-19 21:50:44 +0200

    Added encoding='ISO-8859-1'

--- fbarhelper/transform.py
+++ fbarhelper/transform.py
@@ -3,7 +3,7 @@ import pandas
 
 
 def main():
-    df = pandas.read_csv('C:/code/python3/fbarhelper/tests/testdata/Transactions_2.csv', delimiter=',')
+    df = pandas.read_csv('C:/code/python3/fbarhelper/tests/testdata/Transactions_999_123456789_20210622_092547.csv', delimiter=';', encoding='ISO-8859-1')
     print(df)
 
 
