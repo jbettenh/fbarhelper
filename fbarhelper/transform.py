c87fd3b4a06f504bd65e8ceb0b22b64196b6d3a1
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-09-20 22:53:31 +0200

    Importing and no quotes

--- fbarhelper/transform.py
+++ fbarhelper/transform.py
@@ -5,13 +5,14 @@ import pandas
 
 def main():
 
-    df = pandas.read_csv('C:/code/python3/fbarhelper/tests/testdata/Transactions_3.csv',
+    df = pandas.read_csv('C:/code/python3/fbarhelper/tests/testdata/Transactions_999_123456789_20210622_092547.csv',
                          sep=';',
                          encoding='ISO-8859-1',
                          parse_dates=['Buchungstag'],
                          quoting=csv.QUOTE_NONE
                          )
-    print(df[['Buchungstag', 'Verwendungszweck', 'Soll']])
+    data = df.replace('"', '', regex=True)
+    print(data[['Buchungstag', 'Verwendungszweck', 'Soll', 'Haben']])
 
 
 if __name__ == '__main__':
