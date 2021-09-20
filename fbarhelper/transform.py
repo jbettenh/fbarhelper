f0a944998b1c8719aaf3ed2f4e7d8f50bc7048e6
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-09-20 22:41:07 +0200

    Import without errors

--- fbarhelper/transform.py
+++ fbarhelper/transform.py
@@ -1,14 +1,18 @@
+import csv
 import os
 import pandas
 
 
 def main():
-    df = pandas.read_csv('C:/code/python3/fbarhelper/tests/testdata/Transactions_1.csv',
+
+    df = pandas.read_csv('C:/code/python3/fbarhelper/tests/testdata/Transactions_3.csv',
                          sep=';',
                          encoding='ISO-8859-1',
-                         parse_dates=['Buchungstag']
+                         parse_dates=['Buchungstag'],
+                         quoting=csv.QUOTE_NONE
                          )
-    print(df[['Buchungstag', 'Verwendungszweck', 'Soll', 'Waehrung']])
+    print(df[['Buchungstag', 'Verwendungszweck', 'Soll']])
+
 
 if __name__ == '__main__':
     main()
