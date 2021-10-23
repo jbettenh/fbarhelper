fe78eee5f18060e15f921dd163b2f3d665596704
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-10-23 21:33:30 +0200

    Convert date format

--- fbarhelper/transform.py
+++ fbarhelper/transform.py
@@ -9,10 +9,13 @@ def import_bank_csv(csv_file):
     df = pandas.read_csv(csv_file,
                          sep=';',
                          encoding='ISO-8859-1',
-                         parse_dates=['Buchungstag'],
                          quoting=csv.QUOTE_NONE
                          )
     data = df.replace('"', '', regex=True)
+
+    # Convert from dd.mm.yyyy to yyyy-mm-dd
+    data['Buchungstag'] = pandas.to_datetime(data['Buchungstag'], format='%d.%m.%Y').dt.date
+    data['Wert'] = pandas.to_datetime(data['Wert'], format='%d.%m.%Y').dt.date
 
     return data
 
