e8c94d8660a8f58c6f95edb4275d641ef0c72092
Joseph Bettenhausen <21023218+jbettenh@users.noreply.github.com>
2022-02-11 18:54:06 +0100

    Add menu options

---
+++ fbarhelper/main.py
@@ -0,0 +1,39 @@
+def print_menu(menu):
+    print('What would you like to do? Please enter a number.')
+    for key in menu.keys():
+        print(key, '--', menu[key])
+
+
+def main():
+    menu_options = {
+        1: 'Import CSV files.',
+        2: 'Enter a balance.',
+        3: 'Show max credit.',
+        4: 'Show max debit.',
+        5: 'Exit',
+    }
+    while(True):
+        print_menu(menu_options)
+        selection = ''
+        try:
+            selection = int(input())
+        except:
+            print('Wrong input, please enter a number.')
+
+        if selection == 1:
+            print('1')
+        elif selection == 2:
+            print('2')
+        elif selection == 3:
+            print('3')
+        elif selection == 4:
+            print('4')
+        elif selection == 5:
+            print('Bye!!')
+            exit()
+        else:
+            print('Invalid selection. Please enter a number between 1 and 5.')
+
+
+if __name__ == '__main__':
+    main()
